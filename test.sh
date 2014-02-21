d57f4953653e5134791494901c779b6d0559c2b5
ss4555 <ss4555@columbia.edu>
2014-02-21 10:19:31 -0500

    test script

---
+++ test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash 
+i=50
+start=`date +%s`
+while [ $i -ne 0 ]
+do
+	curl 192.168.56.101:100/testfile.pdf > /dev/null &
+	#curl localhost:1000/images/ship.jpg > /dev/null &
+	i=$[$i-1]
+
+done
+wait
+end=`date +%s`
+dif=$[ end - start ]
+echo $dif
+
+
+#!/bin/bash
+
+# This script opens 4 terminal windows.
+
